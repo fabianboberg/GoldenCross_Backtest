900e1078f18ab84041d258ba63826623d6fbafd8
fabianboberg <fabbe.boberg@gmail.com>
2025-06-12 14:09:14 +0200

    Update FetchCloseDataAAPL.py
    fixed description error

--- FetchCloseDataAAPL.py
+++ FetchCloseDataAAPL.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 
 def get_aapl_stockdata(ticker='AAPL', start="2010-01-01"):
-    stock_data = yf.download(ticker, start) #Fetches apples closing price from 2010-01-01 to today, format is pandas.DataFrame
+    stock_data = yf.download(ticker, start) #Fetches apple stock data from 2010-01-01 to today, format is pandas.DataFrame
     return stock_data
 
